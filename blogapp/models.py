d04cb87e08f0f853f7e09ccf6999eb3dbb8f5092
Abhishek Tiwari <abhiabhishektiwari1106@gmail.com>
2023-11-20 20:07:39 +0530

    Users can now add/edit/delete posts

--- blogapp/models.py
+++ blogapp/models.py
@@ -22,7 +22,7 @@ class User(db.Model, UserMixin):
 
 
 class Post(db.Model):
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False,
                             default=datetime.utcnow)
--- blogapp/routes.py
+++ blogapp/routes.py
@@ -1,72 +1,17 @@
-from flask import render_template, url_for, flash, redirect, request
+import secrets, os
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request, abort
 from blogapp import app, db, bcrypt
-from blogapp.forms import RegistrationForm, LoginForm
+from blogapp.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from blogapp.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-
-posts = [
-    {
-        'author': 'Abhishekt',
-        'title': 'How to Not Die by A Shark',
-        'content': 'Just don''t swim bruh',
-        'date_posted': 'June 11, 2002'
-    },
-
-    {
-        'author': 'Alex Johnson',
-        'title': 'The Power of Positive Thinking',
-        'content': 'Discover how cultivating a positive mindset can impact your life. From fostering resilience to attracting success, explore the benefits of maintaining an optimistic outlook.',
-        'date_posted': 'November 15, 2023'
-    },
-
-    {
-        'author': 'Emily Turner',
-        'title': 'Exploring Hidden Gems: Off-the-Beaten-Path Travel Destinations',
-        'content': 'Escape the ordinary and embark on a journey to lesser-known travel destinations. Uncover the charm of hidden gems that promise unique experiences and unforgettable memories.',
-        'date_posted': 'October 8, 2023'
-    },
-
-    {
-        'author': 'Chris Anderson',
-        'title': 'The Science Behind Healthy Habits',
-        'content': 'Dive into the scientific principles that contribute to forming and maintaining healthy habits. Learn how understanding the brain''s mechanisms can empower you to make positive lifestyle changes.',
-        'date_posted': 'September 25, 2023'
-    },
-
-    {
-        'author': 'Mia Rodriguez',
-        'title': 'Culinary Adventures: Exploring World Cuisines in Your Kitchen',
-        'content': 'Embark on a culinary journey without leaving your home. Explore diverse world cuisines, try new recipes, and elevate your cooking skills to create delicious meals from around the globe.',
-        'date_posted': 'August 12, 2023'
-    },
-
-    {
-        'author': 'Ryan Foster',
-        'title': 'Mindfulness Meditation: A Guide to Inner Peace',
-        'content': 'Learn the art of mindfulness meditation and its profound impact on mental well-being. Discover practical tips and techniques to incorporate mindfulness into your daily routine.',
-        'date_posted': 'July 29, 2023'
-    },
-
-    {
-        'author': 'Sophie Clark',
-        'title': 'The Joy of Reading: Building a Lifelong Reading Habit',
-        'content': 'Unlock the joy of reading and cultivate a lifelong reading habit. Explore book recommendations, tips for creating a reading-friendly environment, and the benefits of regular reading.',
-        'date_posted': 'June 5, 2023'
-    },
-
-    {
-        'author': 'Daniel White',
-        'title': 'Mastering the Art of Time Management',
-        'content': 'Efficiently manage your time with practical strategies and techniques. From prioritization to overcoming procrastination, discover the keys to mastering the art of time management.',
-        'date_posted': 'May 20, 2023'
-    }
-]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -82,7 +27,7 @@ def register():
                     email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash(f'Account Created, You can now Log in!', 'success')
+        flash(f'Your account has been created successfully', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', form=form, title='Register')
 
@@ -108,12 +53,89 @@ def login():
 def about():
     return render_template('about.html', title='About')
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _ , f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='My Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.img_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data =  current_user.email
+    img_file = url_for('static', filename='profile_pics/' +
+                       current_user.img_file)
+    return render_template('account.html', title='My Account', img_file=img_file, form=form)
+
+
+@app.route('/posts/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content  = form.content.data, author = current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form = form, legend = 'New Post')
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash(f'Your post has been successfully updated!', 'success')
+        return redirect(url_for('post', post_id = post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form = form, legend = 'Update Post')
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash(f'Your post has been deleted', 'success')
+    return redirect(url_for('home'))
