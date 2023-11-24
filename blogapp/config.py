92187ceb02a76dbf6800e254bb879f5374e6b141
Abhishek Tiwari <abhiabhishektiwari1106@gmail.com>
2023-11-25 02:36:55 +0530

    new

--- blogapp/config.py
+++ blogapp/config.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = 'my super secret key'
+    # SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'postgres://mrfjugvxzfetrd:4e96fc336363481d99919d509fccd7c3f043fdf8a8da91090388b34be79ec2b7@ec2-54-156-8-21.compute-1.amazonaws.com:5432/da7c29vbu8krn7'
     MAIL_SERVER  = 'smtp.googlemail.com'
     MAIL_PORT  = 587
     MAIL_USE_TLS  = True
