a23d52abb9a876c80a81a87def03f189c142430e
Abhishek Tiwari <abhiabhishektiwari1106@gmail.com>
2023-11-25 11:07:51 +0530

    update config again

--- blogapp/config.py
+++ blogapp/config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = 'my super secret key'
     # SECRET_KEY = os.getenv('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://mrfjugvxzfetrd:4e96fc336363481d99919d509fccd7c3f043fdf8a8da91090388b34be79ec2b7@ec2-54-156-8-21.compute-1.amazonaws.com:5432/da7c29vbu8krn7'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://mpmuxidhaekftp:0969c6b91cea6949f11af3217340d15e79d7d08db88302b130b1610afe6b2b0f@ec2-44-213-228-107.compute-1.amazonaws.com:5432/d92kavj4prbqok'
     MAIL_SERVER  = 'smtp.googlemail.com'
     MAIL_PORT  = 587
     MAIL_USE_TLS  = True
